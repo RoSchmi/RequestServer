3229301452dd5236571abbfde8ea248dfe83e0a7
John <johnbrochue@outlook.com>
2013-09-28 22:11:16 -0400

    Cleaned up ProcessorNode a bit.

--- Game Server/IProcessorNode.cpp
+++ Game Server/IProcessorNode.cpp
@@ -1,4 +1,4 @@
-#include "ProcessorNode.h"
+#include "IProcessorNode.h"
 
 #include <algorithm>
 
@@ -7,7 +7,7 @@ using namespace Utilities;
 using namespace Utilities::Net;
 using namespace GameServer;
 
-ProcessorNode::ProcessorNode(HandlerCreator handlerCreator, ContextCreator contextCreator, string configFileName, ObjectId areaId) {
+IProcessorNode::IProcessorNode(HandlerCreator handlerCreator, ContextCreator contextCreator, string configFileName, ObjectId areaId, void* state) {
 	this->config.readFile(configFileName.c_str());
 	auto& settings = this->config.getRoot();
 
@@ -15,30 +15,31 @@ ProcessorNode::ProcessorNode(HandlerCreator handlerCreator, ContextCreator conte
 	this->handlerCreator = handlerCreator;
 	this->contextCreator = contextCreator;
 	this->areaId = areaId;
+	this->state = state;
 
 	auto& dbSettings = settings["Database"];
 	this->dbParameters = { dbSettings["host"], dbSettings["port"], dbSettings["dbname"], dbSettings["role"], dbSettings["password"] };
 
 	if (this->contextCreator)
 		for (word i = 0; i < this->workers; i++)
-			this->dbConnections[i] = this->contextCreator(this->dbParameters);
+			this->dbConnections[i] = this->contextCreator(this->dbParameters, this->state);
 
-	this->requestServer = RequestServer(vector<string> { settings["tcpServerPort"].c_str(), settings["webSocketServerPort"].c_str() }, vector<bool> { false, true }, this->workers, IResultCode::RETRY_LATER, ProcessorNode::onRequest, nullptr, ProcessorNode::onDisconnect, this);
+		this->requestServer = RequestServer(vector<string> { settings["tcpServerPort"].c_str(), settings["webSocketServerPort"].c_str() }, vector<bool> { false, true }, this->workers, IResultCode::RETRY_LATER, IProcessorNode::onRequestDispatch, IProcessorNode::onConnectDispatch, IProcessorNode::onDisconnectDispatch, this);
 
 	if (this->areaId != 0) {
 		TCPConnection brokerNode(settings["brokerAddress"].c_str(), settings["brokerPort"].c_str(), this);
 		brokerNode.state = reinterpret_cast<void*>(this->areaId);
-		brokerNode.send(reinterpret_cast<uint8*>(&this->areaId), sizeof(this->areaId));
 		this->brokerNode = &this->requestServer.adoptConnection(std::move(brokerNode));
 		this->authenticatedClients[this->areaId].push_back(this->brokerNode);
+		this->sendMessageToBroker(this->areaId, this->createMessage(0x00, 0x00));
 	}
 }
 
-ProcessorNode::~ProcessorNode() {
+IProcessorNode::~IProcessorNode() {
 
 }
 
-void ProcessorNode::sendNotification(ObjectId receipientUserId, DataStream&& notification) {
+void IProcessorNode::sendMessage(ObjectId receipientUserId, DataStream&& notification) {
 	this->clientsLock.lock();
 
 	auto iter = this->authenticatedClients.find(receipientUserId);
@@ -49,51 +50,47 @@ void ProcessorNode::sendNotification(ObjectId receipientUserId, DataStream&& not
 	this->clientsLock.unlock();
 }
 
-void ProcessorNode::sendMessageToBroker(DataStream&& message) {
+void IProcessorNode::sendMessageToBroker(ObjectId targetAreaId, DataStream&& message) {
+	message.write(targetAreaId);
 	this->requestServer.addToOutgoingQueue(RequestServer::Message(*this->brokerNode, std::move(message)));
 }
 
-DataStream ProcessorNode::createNotification(uint8 category, uint8 type) {
+DataStream IProcessorNode::createMessage(uint8 category, uint8 type) {
 	DataStream notification;
 	RequestServer::Message::writeHeader(notification, 0x00, category, type);
 	return notification;
 }
 
-DataStream ProcessorNode::createBrokerMessage(ObjectId targetAreaId, uint8 category, uint8 type) {
-	DataStream notification;
-	notification.write(targetAreaId);
-	RequestServer::Message::writeHeader(notification, 0x00, category, type);
-	return notification;
+void IProcessorNode::onConnect(TCPConnection& client) {
+
 }
 
-void ProcessorNode::onDisconnect(TCPConnection& client, void* state) {
-	ProcessorNode& node = *static_cast<ProcessorNode*>(state);
+void IProcessorNode::onDisconnect(TCPConnection& client) {
 	ObjectId authenticatedId = reinterpret_cast<ObjectId>(client.state);
 
-	node.clientsLock.lock();
+	this->clientsLock.lock();
 
-	auto i = node.authenticatedClients.find(authenticatedId);
-	if (i != node.authenticatedClients.end()) {
+	auto i = this->authenticatedClients.find(authenticatedId);
+	if (i != this->authenticatedClients.end()) {
 		auto& list = i->second;
 		auto j = find(list.begin(), list.end(), &client);
 
 		list.erase(j);
 
 		if (list.size() == 0)
-			node.authenticatedClients.erase(authenticatedId);
+			this->authenticatedClients.erase(authenticatedId);
 	}
 
-	node.clientsLock.unlock();
+	this->clientsLock.unlock();
 }
 
-RequestServer::RequestResult ProcessorNode::onRequest(TCPConnection& client, void* state, word workerNumber, uint8 requestCategory, uint8 requestMethod, DataStream& parameters, DataStream& response) {
-	ProcessorNode& node = *static_cast<ProcessorNode*>(state);
+RequestServer::RequestResult IProcessorNode::onRequest(TCPConnection& client, word workerNumber, uint8 requestCategory, uint8 requestMethod, DataStream& parameters, DataStream& response) {
 	ResultCode resultCode = IResultCode::SUCCESS;
 	ObjectId authenticatedId = reinterpret_cast<ObjectId>(client.state);
 	ObjectId startId = authenticatedId;
-	auto& context = node.contextCreator ? node.dbConnections[workerNumber] : node.emptyDB;
+	auto& context = this->contextCreator ? this->dbConnections[workerNumber] : this->emptyDB;
 
-	auto handler = node.handlerCreator(requestCategory, requestMethod, authenticatedId, context, resultCode);
+	auto handler = this->handlerCreator(requestCategory, requestMethod, authenticatedId, context, resultCode, this->state);
 	if (!handler) {
 		resultCode = IResultCode::INVALID_REQUEST_TYPE;
 		goto end;
@@ -107,7 +104,7 @@ RequestServer::RequestResult ProcessorNode::onRequest(TCPConnection& client, voi
 		goto end;
 	}
 
-	if (node.contextCreator) {
+	if (this->contextCreator) {
 		context->beginTransaction();
 		resultCode = handler->process();
 		try {
@@ -127,9 +124,9 @@ RequestServer::RequestResult ProcessorNode::onRequest(TCPConnection& client, voi
 		return RequestServer::RequestResult::NO_RESPONSE;
 
 	if (authenticatedId != startId) {
-		node.clientsLock.lock();
-		node.authenticatedClients[authenticatedId].push_back(&client);
-		node.clientsLock.unlock();
+		this->clientsLock.lock();
+		this->authenticatedClients[authenticatedId].push_back(&client);
+		this->clientsLock.unlock();
 		client.state = reinterpret_cast<void*>(authenticatedId);
 	}
 
@@ -140,4 +137,16 @@ end:
 		handler->serialize(response);
 
 	return RequestServer::RequestResult::SUCCESS;
+}
+
+RequestServer::RequestResult IProcessorNode::onRequestDispatch(TCPConnection& client, void* state, word workerNumber, uint8 requestCategory, uint8 requestMethod, DataStream& parameters, DataStream& response) {
+	return reinterpret_cast<IProcessorNode*>(state)->onRequest(client, workerNumber, requestCategory, requestMethod, parameters, response);
+}
+
+void IProcessorNode::onDisconnectDispatch(TCPConnection& client, void* state) {
+	reinterpret_cast<IProcessorNode*>(state)->onDisconnect(client);
+}
+
+void IProcessorNode::onConnectDispatch(TCPConnection& client, void* state) {
+	reinterpret_cast<IProcessorNode*>(state)->onConnect(client);
 }
---
+++ Game Server/IProcessorNode.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <string>
+#include <mutex>
+#include <map>
+#include <memory>
+#include <vector>
+
+#include <libconfig.h++>
+
+#include <Utilities/Common.h>
+#include <Utilities/SQLDatabase.h>
+#include <Utilities/RequestServer.h>
+#include <Utilities/TCPConnection.h>
+
+#include "IDBContext.h"
+#include "Common.h"
+
+namespace GameServer {
+	class BaseHandler {
+		public:
+			exported virtual ResultCode process() = 0;
+			exported virtual void deserialize(Utilities::DataStream& parameters) = 0;
+			exported virtual void serialize(Utilities::DataStream& response) = 0;
+	};
+
+	class IProcessorNode {
+		public:
+			typedef std::shared_ptr<BaseHandler>(*HandlerCreator)(uint8 category, uint8 method, ObjectId& userId, std::shared_ptr<IDBContext>& db, uint16& errorCode, void* state);
+			typedef std::shared_ptr<IDBContext>(*ContextCreator)(Utilities::SQLDatabase::Connection::Parameters& parameters, void* state);
+
+			libconfig::Config config;
+
+			exported IProcessorNode(HandlerCreator handlerCreator, ContextCreator contextCreator, std::string configFileName, ObjectId areaId, void* state = nullptr);
+			exported virtual ~IProcessorNode();
+
+			exported void sendMessage(ObjectId receipientUserId, Utilities::DataStream&& notification);
+			exported void sendMessageToBroker(ObjectId targetAreaId, Utilities::DataStream&& message);
+			exported Utilities::DataStream createMessage(uint8 category, uint8 type);
+
+		protected:
+			Utilities::SQLDatabase::Connection::Parameters dbParameters;
+			Utilities::Net::RequestServer requestServer;
+			ObjectId areaId;
+			Utilities::Net::TCPConnection* brokerNode;
+			std::map<ObjectId, std::vector<Utilities::Net::TCPConnection*>> authenticatedClients;
+			std::mutex clientsLock;
+			word workers;
+			void* state;
+			HandlerCreator handlerCreator;
+			ContextCreator contextCreator;
+			std::shared_ptr<IDBContext> emptyDB;
+			std::vector<std::shared_ptr<IDBContext>> dbConnections;
+
+			virtual void onConnect(Utilities::Net::TCPConnection& client);
+			virtual void onDisconnect(Utilities::Net::TCPConnection& client);
+			virtual Utilities::Net::RequestServer::RequestResult onRequest(Utilities::Net::TCPConnection& client, word workerNumber, uint8 requestCategory, uint8 requestMethod, Utilities::DataStream& parameters, Utilities::DataStream& response);
+
+			static void onConnectDispatch(Utilities::Net::TCPConnection& client, void* state);
+			static void onDisconnectDispatch(Utilities::Net::TCPConnection& client, void* state);
+			static Utilities::Net::RequestServer::RequestResult onRequestDispatch(Utilities::Net::TCPConnection& client, void* state, word workerNumber, uint8 requestCategory, uint8 requestMethod, Utilities::DataStream& parameters, Utilities::DataStream& response);
+	};
+}
