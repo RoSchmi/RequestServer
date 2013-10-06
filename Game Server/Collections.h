e46f36ac2e99483e1aab2f05662d1365df80feeb
John <johnbrochue@outlook.com>
2013-10-06 16:57:17 -0400

    Fixed some missed casings.

--- Game Server/Collections.h
+++ Game Server/Collections.h
@@ -2,54 +2,54 @@
 
 #include <string>
 
-#include <Utilities/SQL/Database.h>
-#include <Utilities/SQL/TableBinder.h>
+#include <Utilities/SQL/PostgreSQL.h>
+#include <Utilities/SQL/PostgreSQLBinder.h>
 
-#include "cache_provider.h"
-#include "objects.h"
+#include "CacheProvider.h"
+#include "Objects.h"
 
 namespace game_server {
-	template<typename T> struct IDBCollection {
-		exported IDBCollection(const util::sql::connection& contextConnection, std::string tableName) : dbConnection(contextConnection), tableBinder(tableName, true) { 
+	template<typename T> struct db_collection {
+		exported db_collection(const util::sql::postgres::connection& connection, std::string table_name) : db(connection), binder(table_name, true) { 
 		
 		}
 
-		exported virtual ~IDBCollection() { 
+		exported virtual ~db_collection() { 
 		
 		}
 
 		exported T get_by_id(obj_id id) {
-			return this->tableBinder.executeSelectById(this->dbConnection, id);
+			return this->binder.select_by_id(this->db, id);
 		}
 
 		exported virtual void update(T& object) {
-			this->tableBinder.executeUpdate(this->dbConnection, object);
+			this->binder.update(this->db, object);
 		}
 
 		exported virtual void insert(T& object) {
-			this->tableBinder.executeInsert(this->dbConnection, object);
+			this->binder.insert(this->db, object);
 		}
 
 		exported virtual void remove(T& object) {
-			this->tableBinder.executeDelete(this->dbConnection, object);
+			this->binder.remove(this->db, object);
 		}
 
 		protected:
-			const util::sql::connection& dbConnection;
-			util::sql::table_binder<T> tableBinder;
+			const util::sql::postgres::connection& db;
+			util::sql::postgres::table_binder<T> binder;
 	};
 
-	template<typename T> struct ICachedCollection : public IDBCollection<T> {
-		exported ICachedCollection(const util::sql::connection& contextConnection, cache_provider& contextCache, std::string tableName) : IDBCollection<T>(contextConnection, tableName), cache(contextCache) { 
+	template<typename T> struct cached_collection : public db_collection<T> {
+		exported cached_collection(const util::sql::postgres::connection& connection, cache_provider& cache, std::string table_name) : db_collection<T>(connection, table_name), cache(cache) {
 
 		}
 
-		exported virtual ~ICachedCollection() { 
+		exported virtual ~cached_collection() { 
 		
 		}
 
 		template<typename U> exported void load(std::string fieldName, U fieldValue) {
-			for (auto i : this->tableBinder.executeSelectManyByField(this->dbConnection, fieldName, fieldValue))
+			for (auto i : this->binder.executeSelectManyByField(this->db, fieldName, fieldValue))
 				this->cache.add(new T(i));
 		}
 
@@ -61,26 +61,26 @@ namespace game_server {
 			return dynamic_cast<T*>(this->cache.get_by_location(x, y));
 		}
 
-		exported void update_location(T* object, coord newX, coord newY) {
-			IDBCollection<T>::update(*object);
-			this->cache.update_location(object, newX, newY);
+		exported void update_location(T* object, coord new_x, coord new_y) {
+			db_collection<T>::update(*object);
+			this->cache.update_location(object, new_x, new_y);
 		}
 
-		exported void changePlanet(T* object, obj_id newPlanetId) {
-			object->planet_id = newPlanetId;
-			IDBCollection<T>::update(*object);
-			this->dbConnection.commit_transaction();
+		exported void changePlanet(T* object, obj_id new_planet_id) {
+			object->planet_id = new_planet_id;
+			db_collection<T>::update(*object);
+			this->db.commit_transaction();
 			this->cache.remove(object);
 			delete object;
 		}
 
 		exported void insert(T* object) {
-			IDBCollection<T>::insert(*object);
+			db_collection<T>::insert(*object);
 			this->cache.add(object);
 		}
 
 		exported void remove(T* object) {
-			IDBCollection<T>::remove(object);
+			db_collection<T>::remove(object);
 			this->cache.remove(object);
 			delete object;
 		}
