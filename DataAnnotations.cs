954c4b144f35246f16bdd06e89983808552fa686
Arke64 <johnbrochue@outlook.com>
2015-01-13 20:03:54 -0500

    Added a data annotations namespace.

---
+++ DataAnnotations.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArkeIndustries.RequestServer.DataAnnotations {
+	public class ApiStringAttribute : ValidationAttribute {
+		public int MinLength { get; set; } = 5;
+		public int MaxLength { get; set; } = 100;
+		public bool AllowWhiteSpace { get; set; } = false;
+
+		public override bool IsValid(object value) {
+			var str = (string)value;
+
+			return str.Length >= this.MinLength && str.Length <= this.MaxLength && (this.AllowWhiteSpace || !string.IsNullOrWhiteSpace(str));
+		}
+	}
+}
