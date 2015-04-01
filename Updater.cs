b37713000b9e69a3cfcbd683d9552ec88d435959
Arke64 <johnbrochue@outlook.com>
2015-04-01 19:03:19 -0400

    Refactored updater.

--- Updater.cs
+++ Updater.cs
@@ -4,31 +4,39 @@ using System.Threading.Tasks;
 
 namespace ArkeIndustries.RequestServer {
 	public abstract class Updater<TContext> where TContext : MessageContext {
+		private bool running;
 		private Task worker;
 		private Node<TContext> node;
+		private CancellationTokenSource sleepCanceller;
 
-		public CancellationToken CancellationToken { get; set; }
 		public TimeSpan Interval { get; set; }
 
 		protected TContext Context { get { return this.node.Context; } }
 
 		public Updater(Node<TContext> node) {
 			this.node = node;
+			this.running = false;
 		}
 
 		public void Start() {
-			this.worker = new Task(this.DoWork, this.CancellationToken, TaskCreationOptions.LongRunning);
+			this.running = true;
+			this.sleepCanceller = new CancellationTokenSource();
+
+			this.worker = new Task(this.DoWork, TaskCreationOptions.LongRunning);
 			this.worker.Start();
 		}
 
 		public void Stop() {
+			this.running = false;
+			this.sleepCanceller.Cancel();
+
 			this.worker.Wait();
 		}
 
-		private void DoWork() {
+		private async void DoWork() {
 			var next = DateTime.UtcNow;
 
-			while (!this.CancellationToken.IsCancellationRequested) {
+			while (this.running) {
 				next = next.Add(this.Interval);
 
 				this.node.OnUpdateStarted();
@@ -44,7 +52,7 @@ namespace ArkeIndustries.RequestServer {
 				this.node.OnUpdateFinished();
 
 				try {
-					Task.Delay(next - DateTime.UtcNow, this.CancellationToken).Wait();
+					await Task.Delay(next - DateTime.UtcNow, this.sleepCanceller.Token);
 				}
 				catch (AggregateException e) when (e.InnerExceptions.Count == 1 && e.InnerExceptions[0] is TaskCanceledException) {
 					break;
